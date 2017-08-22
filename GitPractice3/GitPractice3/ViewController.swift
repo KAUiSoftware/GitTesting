cc7f294e44c78d873f6a680a58f39193cdf42b15
Mark Underwood <MarkU@KAUi.com>
2017-08-22 15:40:11 -0400

    Sample of a change

--- GitPractice3/GitPractice3/ViewController.swift
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // This is a test...
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
