870a4cb987b22f9a3fbfb11c7ec9f2900f585603
Mark Underwood <MarkU@KAUi.com>
2017-08-22 14:29:36 -0400

    Upload test

---
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice3
+//
+//  Created by Mark Underwood on 8/22/17.
+//  Copyright © 2017 KAUi Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
